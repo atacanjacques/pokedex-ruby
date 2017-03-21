ca118255d86d4677fc01f0e78d25426d57f87118
atacanjacques <atacan.jacques@gmail.com>
2017-03-21 15:57:05 +0100

    method new

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -6,4 +6,17 @@ class PokemonsController < ApplicationController
 	def show
 		@pokemon = Pokemon.find params[:id]
 	end
+
+	def new
+		@pokemon = Pokemon.new
+	end
+
+	def create
+		@pokemon = Pokemon.new params.require(:pokemon).permit(:name)
+		if @pokemon.save
+			redirect_to @pokemon
+		else
+			render 'new'
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :pokemons, only: [:index, :show]
+  resources :pokemons
 end
