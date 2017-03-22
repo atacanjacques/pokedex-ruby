ba9fb834d4f56790ee7adb53974ef32b36dffd42
atacanjacques <atacan.jacques@gmail.com>
2017-03-22 14:34:41 +0100

    fix add_column migrate

--- db/migrate/20170322131959_add_attributes_to_pokemons.rb
+++ db/migrate/20170322131959_add_attributes_to_pokemons.rb
@@ -1,7 +1,7 @@
 class AddAttributesToPokemons < ActiveRecord::Migration[5.0]
   def change
-  	add column :pokemons, :number, :integer
-  	add column :pokemons, :level, :integer
-  	add column :pokemons, :health_points, :integer
+  	add_column :pokemons, :number, :integer
+  	add_column :pokemons, :level, :integer
+  	add_column :pokemons, :health_points, :integer
   end
 end
