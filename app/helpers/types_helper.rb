f42dbb0b63a6055b21e67100f377c79849c47fa5
atacanjacques <atacan.jacques@gmail.com>
2017-04-13 11:41:49 +0200

    modif helper

--- app/helpers/types_helper.rb
+++ app/helpers/types_helper.rb
@@ -1,7 +1,11 @@
 module TypesHelper
-	def type_badge(name, color)
+	def type_badge(type, label = type.name)
+		link_to(type_badge_content(label, type.color), type).html_safe
+	end
+
+	def type_badge_content(label, color)
 		raw "<span style='background-color: #{color}' class='badge'>
-		#{name}
+		#{label}
 		</span>"
 	end
 end
